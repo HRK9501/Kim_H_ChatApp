43a8882211f0d62a1ea6798422bfcf13c8ee7cc6
hrk9501 <h_kim90825@fanshaweonline.ca>
2020-01-28 15:48:00 -0500

    sprint1

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 
+// import the socket.io
+const io = require('socket.io')(); //instantiate the socket.io library away with the () method -> makes it run
+
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -13,3 +16,17 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+//this is all of our socket.io messaging functionality
+
+//attach socket.io 
+io.attach(server);
+
+io.on('connection', function(secket) {
+    console.log('user connected');
+
+    //listion for a disconnect event
+    socket.on('disconnect', function() {
+        console.log('a user disconnectied');
+    })
+})
